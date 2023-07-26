f4b08c56a56044fd6be3504b0ae30b40086b4cf1
otavioaraujo08 <otavioaraujo490@gmail.com>
2023-07-26 16:36:44 -0300

    temp: update page styles

---
+++ src/App.ts
@@ -0,0 +1,91 @@
+import styled from 'styled-components';
+import skyWallpaper from './assets/images/skyWallpaper.png';
+
+export const AppBox = styled.div`
+    background-image: url(${skyWallpaper});
+    background-size: cover;
+    background-repeat: no-repeat;
+    background-position: center;
+    height: 100vh;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+`;
+
+export const ChoosenBox = styled.div`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    gap: 10rem;
+    height: 30rem;
+    width: 30rem;
+
+    background: rgba(255, 255, 255, 0.55);
+    box-shadow: 0 8px 32px 0 rgba(31, 38, 135, 0.37);
+    backdrop-filter: blur(3.5px);
+    border-radius: 10px;
+    border: 1px solid rgba(255, 255, 255, 0.18);
+`;
+
+export const ChoosenTitle = styled.h1`
+    font-size: 2rem;
+    font-family: 'IBM Plex Sans', sans-serif;
+    font-weight: 600;
+    color: #363636;
+    margin-top: 2rem;
+
+    @media (max-width: 600px) {
+        font-size: 1.5rem;
+    }
+`;
+
+export const ChoosenBoxButtons = styled.div`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    gap: 1rem;
+`;
+
+export const ChoosenButton = styled.button`
+    width: 15rem;
+    height: 2.5rem;
+    border-radius: 0.5rem;
+    background-color: #293ea5;
+    border: none;
+    font-family: 'IBM Plex Sans', sans-serif;
+    font-size: 1rem;
+    font-weight: 550;
+    line-height: 1rem;
+    letter-spacing: 1px;
+    color: #ffffff;
+    cursor: pointer;
+
+    &:hover {
+        background-color: #1e2d7a;
+        transition: 0.3s ease-in-out;
+    }
+
+    @media (max-width: 600px) {
+        width: 50%;
+        max-width: 14rem;
+        font-size: 0.7rem;
+    }
+`;
+
+export const AuxiliarText = styled.p`
+    font-size: 0.9rem;
+    font-family: 'IBM Plex Sans', sans-serif;
+    font-weight: 400;
+    cursor: pointer;
+    color: #46464a;
+
+    &:hover {
+        color: #000000;
+        text-decoration: underline;
+        transition: 0.3s ease-in-out;
+    }
+
+    @media (max-width: 600px) {
+        font-size: 0.7rem;
+    }
+`;
