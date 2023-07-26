d172c9133983d56e829901efbc9e906d1bba0f32
otavioaraujo08 <otavioaraujo490@gmail.com>
2023-07-26 16:36:29 -0300

    temp: update app page

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,44 @@
-import './App.css'
+import {
+    AppBox,
+    AuxiliarText,
+    ChoosenBox,
+    ChoosenBoxButtons,
+    ChoosenButton,
+    ChoosenTitle,
+} from './App.ts';
+import { useState } from 'react';
 
 function App() {
+    const [login, setLogin] = useState(false);
 
-  return (
-    <>
-      hello word
-    </>
-  )
+    const handleSetLogin = () => {
+        setLogin(!login);
+    };
+
+    const handleRedirectUser = () => {
+        window.location.href = 'https://www.google.com.br';
+    };
+
+    return (
+        <AppBox>
+            {login ? (
+                <h1>Home</h1>
+            ) : (
+                <ChoosenBox>
+                    <ChoosenTitle>Você deseja realizar login ?</ChoosenTitle>
+
+                    <ChoosenBoxButtons>
+                        <ChoosenButton onClick={handleSetLogin}>
+                            Realizar login
+                        </ChoosenButton>
+                        <AuxiliarText onClick={handleRedirectUser}>
+                            Redirecionar para página principal
+                        </AuxiliarText>
+                    </ChoosenBoxButtons>
+                </ChoosenBox>
+            )}
+        </AppBox>
+    );
 }
 
-export default App
+export default App;
