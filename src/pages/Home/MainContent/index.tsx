ed1ec4498e2d2d73094be7e2d5085797163f74e6
otavioaraujo08 <otavioaraujo490@gmail.com>
2023-07-27 17:45:58 -0300

    feat: update main content component page

--- src/pages/Home/MainContent/index.tsx
+++ src/pages/Home/MainContent/index.tsx
@@ -1,10 +1,80 @@
+import { PatternButton } from '../../../components/PatternButton';
 import { Header } from '../../../layout/Header';
-import { Container } from './styles';
+import {
+    AnimeCriticalDiv,
+    AnimeDescription,
+    AnimeDetail,
+    AnimeDetailValue,
+    AnimeDetailsDiv,
+    AnimeImage,
+    AnimeInformationsDiv,
+    AnimeTitle,
+    Container,
+} from './styles';
+import { ArrowSquareRight } from '@phosphor-icons/react';
+import FateBabylonia from '../../../assets/images/fateGrandOrderBabylonia.jpg';
+import FateCamelot from '../../../assets/images/fateGrandOrderCamelot.jpeg';
+import FateSingularity from '../../../assets/images/FateGrandOrderSingularity.webp';
 
 export const MainContent = () => {
     return (
         <Container>
             <Header />
+
+            <AnimeInformationsDiv>
+                <AnimeTitle>Fate/ Grand Order</AnimeTitle>
+
+                <AnimeDescription>
+                    A Guerra do Santo Graal: uma violenta batalha entre magos em
+                    que sete mestres e seus servos convocados lutam pelo Santo
+                    Graal, um artefato mágico que pode realizar qualquer desejo
+                    do vencedor.
+                </AnimeDescription>
+
+                <PatternButton
+                    text="Infomações"
+                    style={{
+                        backgroundColor: '#F2F2F2',
+                        color: '#210303',
+                        border: '2px solid #210303',
+                    }}
+                >
+                    <ArrowSquareRight size={20} color="#210303" weight="bold" />
+                </PatternButton>
+            </AnimeInformationsDiv>
+
+            <AnimeDetailsDiv>
+                <AnimeCriticalDiv>
+                    <AnimeDetail>Classificação:</AnimeDetail>
+
+                    <AnimeDetailValue>7.7</AnimeDetailValue>
+
+                    <AnimeDetail>Gênero:</AnimeDetail>
+
+                    <AnimeDetailValue>fantasia</AnimeDetailValue>
+
+                    <AnimeDetail>Status</AnimeDetail>
+
+                    <AnimeDetailValue>Finalizado</AnimeDetailValue>
+                </AnimeCriticalDiv>
+
+                <AnimeCriticalDiv hideOnMobile={true}>
+                    <AnimeImage
+                        alt="Fate/ Grand Order image"
+                        src={FateBabylonia}
+                    />
+
+                    <AnimeImage
+                        alt="Fate/ Grand Order image"
+                        src={FateCamelot}
+                    />
+
+                    <AnimeImage
+                        alt="Fate/ Grand Order image"
+                        src={FateSingularity}
+                    />
+                </AnimeCriticalDiv>
+            </AnimeDetailsDiv>
         </Container>
     );
 };
--- src/pages/Home/MainContent/styles.ts
+++ src/pages/Home/MainContent/styles.ts
@@ -1,6 +1,10 @@
 import AnimeWallpaper from '../../../assets/images/saberWallpaper.jpg';
 import { styled } from 'styled-components';
 
+interface AnimeImageProps {
+    hideOnMobile?: boolean;
+}
+
 export const Container = styled.div`
     height: 100vh;
     width: 100%;
@@ -8,4 +12,70 @@ export const Container = styled.div`
     background-size: cover;
     background-position: center;
     background-repeat: no-repeat;
+    display: flex;
+    flex-direction: column;
+    gap: 3rem;
+`;
+
+export const AnimeInformationsDiv = styled.div`
+    display: flex;
+    flex-direction: column;
+    gap: 1rem;
+    margin-left: 5rem;
+    margin-top: 2.5rem;
+`;
+
+export const AnimeTitle = styled.h1`
+    width: 30%;
+    font-size: 3rem;
+    font-weight: 600;
+    color: #ffffff;
+    font-family: 'Noto Sans', sans-serif;
+`;
+
+export const AnimeDescription = styled.h3`
+    width: 40%;
+    font-size: 1rem;
+    font-weight: 500;
+    line-height: 1.4rem;
+    color: #c4c4c4;
+    font-family: 'Noto Sans', sans-serif;
+`;
+
+export const AnimeDetailsDiv = styled.div`
+    display: flex;
+    justify-content: left;
+    align-items: center;
+    gap: 5rem;
+    margin-left: 5rem;
+`;
+
+export const AnimeCriticalDiv = styled.div<AnimeImageProps>`
+    display: flex;
+    text-align: center;
+    gap: 1rem;
+
+    ${(props) => props.hideOnMobile && 'overflow: hidden;'}
+    ${(props) => !props.hideOnMobile && 'flex-wrap: wrap;'}
+`;
+
+export const AnimeDetail = styled.h3`
+    font-size: 1rem;
+    font-weight: 500;
+    color: #c4c4c4;
+    font-family: 'Noto Sans', sans-serif;
+`;
+
+export const AnimeDetailValue = styled.h3`
+    font-size: 1rem;
+    font-weight: 600;
+    color: #ffb703;
+    font-family: 'Noto Sans', sans-serif;
+`;
+
+export const AnimeImage = styled.img`
+    width: 10rem;
+    height: 7rem;
+    border-radius: 0.5rem;
+    object-fit: cover;
 `;
