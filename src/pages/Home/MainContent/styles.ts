f07c0e071466f65e644e05a0a45595dd632b42cb
otavioaraujo08 <otavioaraujo490@gmail.com>
2023-07-27 18:20:06 -0300

    fix: remove unused interface and update responsivity

--- src/pages/Home/MainContent/styles.ts
+++ src/pages/Home/MainContent/styles.ts
@@ -1,12 +1,9 @@
 import AnimeWallpaper from '../../../assets/images/saberWallpaper.jpg';
 import { styled } from 'styled-components';
 
-interface AnimeImageProps {
-    hideOnMobile?: boolean;
-}
-
 export const Container = styled.div`
-    height: 100vh;
+    min-height: 100vh;
+    height: 100%;
     width: 100%;
     background-image: url(${AnimeWallpaper});
     background-size: cover;
@@ -20,7 +17,7 @@ export const Container = styled.div`
 export const AnimeInformationsDiv = styled.div`
     display: flex;
     flex-direction: column;
-    gap: 1rem;
+    gap: 1.5rem;
     margin-left: 5rem;
     margin-top: 2.5rem;
 `;
@@ -31,15 +28,23 @@ export const AnimeTitle = styled.h1`
     font-weight: 600;
     color: #ffffff;
     font-family: 'Noto Sans', sans-serif;
+
+    @media (max-width: 768px) {
+        font-size: 2.4rem;
+    }
 `;
 
 export const AnimeDescription = styled.h3`
-    width: 40%;
+    width: 50%;
     font-size: 1rem;
     font-weight: 500;
     line-height: 1.4rem;
     color: #c4c4c4;
     font-family: 'Noto Sans', sans-serif;
+
+    @media (max-width: 768px) {
+        font-size: 0.8rem;
+    }
 `;
 
 export const AnimeDetailsDiv = styled.div`
@@ -48,15 +53,35 @@ export const AnimeDetailsDiv = styled.div`
     align-items: center;
     gap: 5rem;
     margin-left: 5rem;
+
+    @media (max-width: 768px) {
+        flex-direction: column;
+        margin-left: 0;
+        gap: 2rem;
+    }
 `;
 
-export const AnimeCriticalDiv = styled.div<AnimeImageProps>`
+export const AnimeCriticalDiv = styled.div`
     display: flex;
+    text-wrap: wrap;
     text-align: center;
     gap: 1rem;
 
-    ${(props) => props.hideOnMobile && 'overflow: hidden;'}
-    ${(props) => !props.hideOnMobile && 'flex-wrap: wrap;'}
+    @media (max-width: 600px) {
+        gap: 2rem;
+        flex-direction: column;
+    }
+`;
+
+export const AnimeCriticalDivImage = styled.div`
+    display: flex;
+    text-align: center;
+    gap: 1rem;
+    overflow: hidden;
+
+    @media (max-width: 768px) {
+        flex-direction: column;
+    }
 `;
 
 export const AnimeDetail = styled.h3`
@@ -64,6 +89,10 @@ export const AnimeDetail = styled.h3`
     font-weight: 500;
     color: #c4c4c4;
     font-family: 'Noto Sans', sans-serif;
+
+    @media (max-width: 600px) {
+        font-size: 0.9rem;
+    }
 `;
 
 export const AnimeDetailValue = styled.h3`
@@ -71,6 +100,10 @@ export const AnimeDetailValue = styled.h3`
     font-weight: 600;
     color: #ffb703;
     font-family: 'Noto Sans', sans-serif;
+
+    @media (max-width: 768px) {
+        font-size: 0.9rem;
+    }
 `;
 
 export const AnimeImage = styled.img`
@@ -78,4 +111,8 @@ export const AnimeImage = styled.img`
     height: 7rem;
     border-radius: 0.5rem;
     object-fit: cover;
+
+    @media (max-width: 768px) {
+        margin: 0.5rem 0;
+    }
 `;
